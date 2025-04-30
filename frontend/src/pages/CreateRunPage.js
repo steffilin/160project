ff472c749f671cd528ecc89aff6ae5b58805b59f
steffilin <steffi2002.lin@gmail.com>
2025-04-30 13:50:33 -0700

    added input fields to create run page

--- frontend/src/pages/CreateRunPage.js
+++ frontend/src/pages/CreateRunPage.js
@@ -1,11 +1,34 @@
 
-import React from 'react';
+import React, { createElement } from 'react';
+import React, { useState } from 'react';
 
 function CreateRunPage() {
+  // const button = ("button");
+  // button.innerHtml=
+  const [eventName, setEventName] = useState('');
+  const [location, setLocation] = useState('');
+  const [time, setTime] = useState('');
+
+  const handleClick = () => {
+    console.log('Select Runners button clicked!');
+    // Add your logic here
+    console.log('Event Name:', eventName);
+    console.log('Location:', location);
+    console.log('Time:', time);
+
+  };
+
   return (
     <div>
-      <h2>Create Run</h2>
-      <p>run run run run </p>
+      <h2>Create Party Run</h2>
+      <h4>Event Name</h4>
+      <input id="event-name" placeholder='Event Name' value={eventName} onChange={(e) => setEventName(e.target.value)}></input>
+      <h4>Location</h4>
+      <input id="location" placeholder='Location' value={location} onChange={(e) => setLocation(e.target.value)}></input>
+      <h4>Time</h4>
+      <input id="time" placeholder='Time' value={time} onChange={(e) => setTime(e.target.value)}></input>
+
+      <button id="btn-select-runner" onClick={handleClick}>Select Runners</button>
     </div>
   );
 }
